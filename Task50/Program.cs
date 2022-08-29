ac7c7b39048ab2c205c4243fa61384ec7cd42942
EfimovE <efimov_e@mail.ru>
2022-08-29 22:32:16 +0500

    save01

--- Task50/Program.cs
+++ Task50/Program.cs
@@ -22,6 +22,27 @@ int[,] CreateMatrixRndInt (int row, int col, int min, int max)
     return matrix;
 }
 
+void CheckPositionMatrix (int elemRow, int elemCol, int[,] array2D)
+{
+    Console.Write($"{elemRow} , {elemCol} -> ");
+    for (int i = 0; i < array2D.GetLength(0); i++)
+    {
+        for (int j = 0; j < array2D.GetLength(1); j++)
+        {
+            // int size = array2D.GetLength(1);
+            // Console.WriteLine($"{size} size"); 
+            if ((elemRow > array2D.GetLength(0)) || (elemCol > array2D.GetLength(1)))
+            {
+                Console.WriteLine("такого элемента не существует в массиве"); 
+                return;
+            } 
+            // if (elemCol > array2D.GetLength(1)) Console.WriteLine("такого элемента нет в массиве"); return;
+            else Console.WriteLine($"{array2D[elemRow - 1, elemCol - 1]}");
+            return;  
+        }
+    }
+}
+
 // Метод для вывода в консоль двумерного массива.
 void PrintMatrix (int [,] matrix)
 {
@@ -47,3 +68,8 @@ Console.WriteLine("Введите максимальное значение эл
 int maxElem = Convert.ToInt32(Console.ReadLine());
 int [,] array2D = CreateMatrixRndInt (m, n, minElem, maxElem);
 PrintMatrix (array2D);
+Console.WriteLine("Введите номер строки массива и нажмите Enter.");
+int elRow = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите номер столбца массива и нажмите Enter.");
+int elCol = Convert.ToInt32(Console.ReadLine());
+CheckPositionMatrix (elRow, elCol, array2D);
